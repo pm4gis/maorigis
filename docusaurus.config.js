a57acd85a22c2bfb6fe87871f90f369c3ac0af34
Duane Wilkins <duane.wilkins@gmail.com>
2026-01-25 18:43:24 +1300

    Fix Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,67 +1,53 @@
 // @ts-check
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Māori GIS Mapping',
-  tagline: 'A practical GIS Mapping guide for those working with an in iwi and Māori communities',
+  tagline: 'A practical GIS mapping guide for people working in iwi and Māori communities',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
   url: 'https://maorigis.pages.dev',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  // This is not GitHub Pages, but Docusaurus still uses these values for some links/plugins.
+  organizationName: 'pm4gis',
+  projectName: 'maorigis',
 
   onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
 
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [{ from: '/', to: '/docs/intro' }],
+      },
+    ],
+  ],
+
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      /** @type {import('@docusaurus/preset-classic').Options} */ ({
         docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: 'docs',
+          editUrl: 'https://github.com/pm4gis/maorigis/edit/main/',
         },
-       
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -70,70 +56,40 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */ ({
       colorMode: {
         respectPrefersColorScheme: true,
       },
-      
-navbar: {
-  title: 'Māori GIS',
-  items: [
-    {
-      type: 'doc',
-      docId: 'intro',
-      position: 'left',
-      label: 'Guide',
-    },
-  ],
-},
-   footer: {
-  style: 'dark',
-  links: [
-    {
-      title: 'About',
-      items: [
-        {
-          label: 'MāoriGIS',
-          to: '/docs/introduction',
-        },
-        {
-          label: 'Ngā Poutama Matawhenua at LINZ',
-          href: 'https://www.linz.govt.nz/poutama',
-        },
-      ],
-    },
-    {
-      title: 'Contact',
-      items: [
-        {
-          label: 'Duane Wilkins',
-          href: 'mailto:duane.wilkins@gmail.com',
-        },
-        {
-          label: 'duane.wilkins@gmail.com',
-          href: 'mailto:duane.wilkins@gmail.com',
-        },
-      ],
-    },
-    {
-      title: 'Connect',
-      items: [
-        {
-          label: 'LinkedIn',
-          href: 'https://www.linkedin.com/in/duanewilkins/',
-        },
-        {
-          label: 'GitHub',
-          href: 'https://github.com/maorigis',
-        },
-      ],
-    },
-  ],
-  copyright: `© ${new Date().getFullYear()} MāoriGIS. He mapi nā tātou, mō tātou.`,
-},
+
+      navbar: {
+        title: 'Māori GIS',
+        items: [{ type: 'doc', docId: 'intro', position: 'left', label: 'Guide' }],
+      },
+
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'About',
+            items: [
+              { label: 'Introduction', to: '/docs/intro' },
+              { label: 'Ngā Poutama Matawhenua at LINZ', href: 'https://www.linz.govt.nz/poutama' },
+            ],
+          },
+          {
+            title: 'Contact',
+            items: [
+              { label: 'Duane Wilkins', href: 'mailto:duane.wilkins@gmail.com' },
+              { label: 'LinkedIn', href: 'https://www.linkedin.com/in/duanewilkins/' },
+            ],
+          },
+          {
+            title: 'Code',
+            items: [{ label: 'GitHub', href: 'https://github.com/pm4gis/maorigis' }],
+          },
+        ],
+        copyright: `© ${new Date().getFullYear()} Māori GIS. He mapi nā tātou, mō tātou.`,
+      },
 
       prism: {
         theme: prismThemes.github,
